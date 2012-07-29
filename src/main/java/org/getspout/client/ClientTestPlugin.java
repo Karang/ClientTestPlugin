db968649446bbec0d78d7f2a7e3d438a01a4285b
RoyAwesome <madKylegar@gmail.com>
2012-07-29 16:22:42 -0700

    Initial commit of the Client Test Plugin

---
+++ src/main/java/org/getspout/client/ClientTestPlugin.java
@@ -0,0 +1,19 @@
+package org.getspout.client;
+
+import org.spout.api.plugin.CommonPlugin;
+
+public class ClientTestPlugin extends CommonPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
