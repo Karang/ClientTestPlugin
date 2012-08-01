595e3bd1123b57c51072e8364f8924f7d2b38750
Chris Sanders <NinjaZidane@hotmail.com>
2012-08-01 00:41:11 -0400

    Cleanup of ClientTestPlugin, proper maven setup, readme, header, license. Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

--- src/main/java/test/plugins/client/entities/TestPlayer.java
+++ src/main/java/test/plugins/client/entities/TestPlayer.java
@@ -1,6 +1,30 @@
+/*
+ * This file is part of ClientTestPlugin.
+ *
+ * Copyright (c) 2012, SpoutDev <http://www.spout.org/>
+ * ClientTestPlugin is licensed under the SpoutDev License Version 1.
+ *
+ * ClientTestPlugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * ClientTestPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
 package test.plugins.client.entities;
-
-import net.royawesome.jlibnoise.MathHelper;
 
 import org.spout.api.entity.component.Controller;
 import org.spout.api.entity.component.controller.PlayerController;
@@ -10,36 +34,32 @@ import org.spout.api.player.Player;
 import org.spout.api.player.PlayerInputState;
 
 public class TestPlayer extends Controller implements PlayerController {
-
 	protected TestPlayer(ControllerType type) {
 		super(type);
-		// TODO Auto-generated constructor stub
 	}
 
+	@Override
 	public Player getPlayer() {
-		// TODO Auto-generated method stub
-		return null;
+		return (Player) this;
 	}
 
 	@Override
 	public void onAttached() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void onTick(float dt){
-		PlayerInputState input = getPlayer().input();
-		
-		Point pos = getParent().getTransform().getPosition();
-		
-		
-		if(input.getForward()) {
-			pos = pos.add(getParent().getTransform().forwardVector());
-		}
-		if(input.getBackward()) {
-			pos = pos.subtract(getParent().getTransform().forwardVector());
-		}
-		
+
 	}
 
+	@Override
+	public void onTick(float dt) {
+		PlayerInputState input = getPlayer().input();
+
+		Point pos = getParent().getTransform().getPosition();
+
+		if (input.getForward()) {
+			pos = pos.add(getParent().getTransform().forwardVector());
+		}
+
+		if (input.getBackward()) {
+			pos = pos.subtract(getParent().getTransform().forwardVector());
+		}
+	}
 }
